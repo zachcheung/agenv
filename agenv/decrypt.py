698bff3016969933f3b08f04bdd1a148dc3d474b
Zach Cheung <kuroro.zhang@gmail.com>
2025-02-06 13:18:02 -0800

    use tempfile for AGE_SECRET_KEY case

--- agenv/decrypt.py
+++ agenv/decrypt.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import tempfile
 from io import StringIO
 
 from dotenv import load_dotenv
@@ -9,11 +10,15 @@ DEFAULT_IDENTITY = os.path.expanduser("~/.age/key")
 
 def decrypt(file: str, identity: str = "") -> str:
     """Decrypts an .age encrypted file and returns its content as a string."""
-    cmd = ""
+    temp_key_file_path = ""
+
     if not identity:
         age_secret_key = os.getenv("AGE_SECRET_KEY")
         if age_secret_key:
-            cmd = f"age -d -i <(echo {age_secret_key}) '{file}'"
+            with tempfile.NamedTemporaryFile(mode="w", delete=False) as fp:
+                fp.write(age_secret_key)
+                temp_key_file_path = fp.name
+                identity = temp_key_file_path
         else:
             age_secret_key_file = os.getenv("AGE_SECRET_KEY_FILE")
             if age_secret_key_file:
@@ -21,14 +26,16 @@ def decrypt(file: str, identity: str = "") -> str:
             else:
                 identity = DEFAULT_IDENTITY
 
-    if not cmd:
-        cmd = f"age -d -i '{identity}' '{file}'"
+    cmd = f"age -d -i '{identity}' '{file}'"
 
     try:
         result = subprocess.run(cmd, shell=True, capture_output=True, text=True, check=True)
         return result.stdout
     except subprocess.CalledProcessError as e:
         raise RuntimeError(f"Decryption failed: {e.stderr.strip()}") from e
+    finally:
+        if temp_key_file_path and os.path.exists(temp_key_file_path):
+            os.remove(temp_key_file_path)
 
 
 def load_age_env(envfile: str = ".env.age", identity: str = "") -> None:
